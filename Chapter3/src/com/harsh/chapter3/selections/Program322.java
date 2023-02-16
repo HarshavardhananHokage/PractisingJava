cec50802e17a45768961ff184c7e4a26659041ed
Harsh <harshavardhan2990@gmail.com>
2023-02-16 21:03:24 +0530

    Updates

---
+++ Chapter3/src/com/harsh/chapter3/selections/Program322.java
@@ -0,0 +1,33 @@
+package com.harsh.chapter3.selections;
+
+import java.util.Scanner;
+
+public class Program322 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter point x: ");
+		int x = scanner.nextInt();
+		
+		System.out.println("Enter point y: ");
+		int y = scanner.nextInt();
+		
+		isPointInsideCircle(x, y);
+		
+		scanner.close();
+	}
+
+	private static void isPointInsideCircle(int x, int y) {
+		
+		double distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+		
+		if(distance <= 10) {
+			System.out.println("Within circle");
+		} else {
+			System.out.println("Outside circle");
+		}
+	}
+
+}
---
+++ Chapter3/src/com/harsh/chapter3/selections/Program323.java
@@ -0,0 +1,34 @@
+package com.harsh.chapter3.selections;
+
+import java.util.Scanner;
+
+public class Program323 {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter point x: ");
+		double x = scanner.nextDouble();
+
+		System.out.println("Enter point y: ");
+		double y = scanner.nextDouble();
+
+		isPointInsideRectangle(x, y);
+		
+		scanner.close();
+	}
+
+	private static void isPointInsideRectangle(double x, double y) {
+
+		double horizontalDistance = Math.sqrt(Math.pow(x, 2));
+		double verticalDistance = Math.sqrt(Math.pow(y, 2));
+
+		if ((horizontalDistance <= (10 / 2)) && (verticalDistance <= (5 / 2))) {
+			System.out.println("Within rectangle");
+		} else {
+			System.out.println("Outside rectangle");
+		}
+
+	}
+}
