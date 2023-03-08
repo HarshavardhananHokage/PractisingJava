1731b2991d64e59b76e99d7ed91b1c93a6cb310c
Harsh <harshavardhan2990@gmail.com>
2023-03-08 12:27:21 +0530

    Updates

---
+++ Chapter3/src/com/harsh/chapter3/selections/Program_03_24.java
@@ -0,0 +1,26 @@
+package com.harsh.chapter3.selections;
+
+import java.util.Random;
+
+public class Program_03_24 {
+
+	public enum Cards {
+		ACE, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING;
+	}
+	
+	public enum Suit {
+		CLUBS, DIAMONDS, HEARTS, SPADES;
+	}
+	
+	public static void main(String[] args) {
+		Random random = new Random();
+		
+		int cardRandom = random.nextInt(13);
+		int suitRandom = random.nextInt(4);
+		
+		System.out.println("cardRandom: " + cardRandom + " Suit Random: " +suitRandom);
+		
+		System.out.printf("You have picked the " +Cards.values()[cardRandom] + " of " +Suit.values()[suitRandom]);
+		
+	}
+}
